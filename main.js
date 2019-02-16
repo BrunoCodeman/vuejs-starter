4c7cdbbfe39ce1f96545d9af59edaa1483728406
Bruno Codeman <brunobemfica@gmail.com>
2019-02-16 00:10:57 -0300

    - changing green socks image path on main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: "#app",
     data: {
             product: "Socks",
-            image: "./socks.jpg",
+            image: "./green_socks.jpg",
             inventory: 100,
             details: ["80% cotton", "20% polyester", "Made by the best people of the world, the proletarians."],
             variants: [{id: 1, color: "Green"},{id: 2, color: "Blue"}],
