16fe8d4dc71d14e3a5fb5c2ff4c415c75021b112
Bruno Codeman <brunobemfica@gmail.com>
2019-02-16 00:23:46 -0300

    -adding image change on product variant mouseover

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ var app = new Vue({
     el: "#app",
     data: {
             product: "Socks",
-            image: "./green_socks.jpg",
+            image: "green_socks.jpg",
             inventory: 100,
             details: ["80% cotton", "20% polyester", "Made by the best people of the world, the proletarians."],
-            variants: [{id: 1, color: "Green"},{id: 2, color: "Blue"}],
+            variants: [{id: 1, color: "green"},{id: 2, color: "blue"}],
             cart: 0
           },
     methods: {
-      addToCart: function() {this.cart+=1}
+      addToCart: function() {this.cart+=1},
+      updateProduct: function (colour) { this.image = colour + "_socks.jpg"; }
+
     },
 });
