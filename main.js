b49f166fadad37e9888c5c57c525bf742ae1203c
Bruno Codeman <brunobemfica@gmail.com>
2019-02-18 15:09:07 -0300

    adding computed properties and methods to manipulate style according to availability in stock

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var app = new Vue({
     el: "#app",
     data: {
+            brand: "SomeFactory",
             product: "Socks",
-            image: "green_socks.jpg",
+            selectedVariant: 0,
             inventory: 100,
             details: ["80% cotton", "20% polyester", "Made by the best people of the world, the proletarians."],
-            variants: [{id: 1, color: "green"},{id: 2, color: "blue"}],
+            variants: [{id: 1, color: "green", quantity: 10},{id: 2, color: "blue", quantity: 0}],
             cart: 0
           },
     methods: {
       addToCart: function() {this.cart+=1},
-      updateProduct: function (colour) { this.image = colour + "_socks.jpg"; }
+      updateProduct: function (index) { this.selectedVariant = index; }
 
     },
+    computed: {
+      title(){ return this.brand + " " + this.product },
+      image() { return this.variants[this.selectedVariant].color + "_socks.jpg"; },
+      InStock() { return this.variants[this.selectedVariant].quantity;}  
+    },
+
 });
